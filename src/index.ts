8fd461774f99022d0a4d47671cd4d617c897a5fb
Thomas Musial <thomaslmusial@gmail.com>
2025-03-22 18:26:20 -0300

    exporting types as well

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export {default as ExperienceCard} from './components/contentPieces/experienceCa
 export {default as ImageTextBox } from './components/contentPieces/imageTextBox/imageTextBox'
 
 export {default as ImageAspects} from './components/textComponents/imageAspects/imageAspects'
+
 export {default as TextAndList} from './components/textComponents/textAndList/textAndList'
+export type { TextAndListProps } from "./components/textComponents/textAndList/textAndList";
